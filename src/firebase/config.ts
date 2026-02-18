72dc255b5e97f52df3876fb13aa5e869ac5ee664
Vicki Bealman <drvicki.bealman@gmail.com>
2026-02-18 19:06:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1650950994-d2820",
+  "appId": "1:555524715072:web:9df4dad7f395c0b6b525a5",
+  "apiKey": "AIzaSyC-mQCiwZKVTTSxE1BN3OaZ13CrwtfqwFI",
+  "authDomain": "studio-1650950994-d2820.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "555524715072"
+};
